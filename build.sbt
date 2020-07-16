b3d8875285cbc98722e43351974d9025cbd25aa7
Mars Liu <mars.liu@outlook.com>
2020-07-16 23:42:15 +0800

    Hello Repl

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "sisp"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
+
+libraryDependencies ++= Seq(
+  "io.github.marchliu" % "jaskell-core_2.13" % "0.5.4"
+)
---
+++ src/main/scala/sisp/Repl.scala
@@ -0,0 +1,15 @@
+package sisp
+
+/**
+ * TODO
+ *
+ * @author mars
+ * @version 1.0.0
+ * @since 2020/07/16 22:20
+ */
+object Repl {
+  val prmt = ">> "
+  def main(args: Array[String]) {
+    print(prmt)
+  }
+}
