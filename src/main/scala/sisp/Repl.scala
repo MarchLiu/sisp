665cef20c4b3e185b5066ad24de14a484e117df3
Mars Liu <mars.liu@outlook.com>
2020-07-19 21:49:12 +0800

    restart from repl

--- src/main/scala/sisp/Repl.scala
+++ src/main/scala/sisp/Repl.scala
@@ -1,10 +1,8 @@
 package sisp
 
-import jaskell.expression.Env
+import jaskell.parsec.State
 
 import scala.io.StdIn.readLine
-import jaskell.expression.parsers.Parser
-import jaskell.parsec.State
 
 /**
  * TODO
@@ -17,19 +15,11 @@ object Repl {
   import jaskell.parsec.Txt._
 
   val prmt = ">> "
-  val parser = new Parser()
-  val env = new Env()
   def main(args: Array[String]) {
     while (true) {
       print(prmt)
       val line:State[Char] = readLine()
 
-      (parser ? line) flatMap {_.makeAst eval env} match {
-        case Right(re) =>
-          println(re)
-        case Left(error) =>
-          println(s"$line parse error ${error.getMessage}")
-      }
     }
   }
 }
