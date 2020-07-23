286bfc84d389664a4f1a3568e43b22433a234086
Mars Liu <mars.liu@outlook.com>
2020-07-23 16:57:15 +0800

    upgrade repl

--- src/main/scala/sisp/Repl.scala
+++ src/main/scala/sisp/Repl.scala
@@ -1,7 +1,8 @@
 package sisp
 
 import jaskell.parsec.ParsecException
-import sisp.parsers.NumberParser
+import sisp.ast.{Add, Divide, Element, Env, Sub}
+import sisp.parsers.{NumberParser, Parser}
 
 import scala.io.StdIn.readLine
 
@@ -13,17 +14,27 @@ import scala.io.StdIn.readLine
  * @since 2020/07/16 22:20
  */
 object Repl {
+
   import jaskell.parsec.Txt._
-  val parser = new NumberParser
+
+  val parser = new Parser
   val prmt = ">> "
+
+  val env = new Env;
+  env.put("+", new Add)
+  env.put("-", new Sub)
+  env.put("*", new sisp.ast.Product)
+  env.put("/", new Divide)
+
   def main(args: Array[String]) {
     while (true) {
       print(prmt)
       val line = readLine()
       parser ask line match {
-        case Right(result) => println(result)
+        case Right(ast) => ast.asInstanceOf[Element].eval(env) foreach println
         case Left(error) => println(s"invalid number [$line] error [${error.getMessage}]")
       }
     }
   }
+
 }
