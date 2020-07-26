011f54a3c3aa9df537886ea05ff2aedbfe363dc8
Mars Liu <mars.liu@outlook.com>
2020-07-26 14:45:47 +0800

    code clean

--- src/main/scala/sisp/parsers/StringParser.scala
+++ src/main/scala/sisp/parsers/StringParser.scala
@@ -1,9 +1,9 @@
 package sisp.parsers
 
-import jaskell.parsec.{Is, Parsec, State}
-import jaskell.parsec.Combinator.{between, many, attempt}
-import jaskell.parsec.Atom.{is, one}
-import jaskell.parsec.Txt.{ch, mkString}
+import jaskell.parsec.Atom.one
+import jaskell.parsec.Combinator.{attempt, between, many}
+import jaskell.parsec.Txt.{ch, chNone, mkString}
+import jaskell.parsec.{ChNone, Parsec, State}
 import sisp.ParserException
 
 /**
@@ -14,8 +14,9 @@ import sisp.ParserException
  * @since 2020/07/24 15:55
  */
 class StringParser extends Parsec[Any, Char] {
-  val predicate: Char => Boolean = { c: Char => c != '"' && c != '\\' }
-  val char: Parsec[Char, Char] = is(predicate)
+  // val predicate: Char => Boolean = { c: Char => c != '"' && c != '\\' }
+  // val char: Parsec[Char, Char] = is(predicate)
+  val char: ChNone = chNone("\"\\")
   val escapeChar: Parsec[Char, Char] = attempt(ch('\\') >> { s =>
     one ? s flatMap {
       case 'n' => Right('\n')
