8684cbc5830f6c9409cc13ebbf169aa5720eb499
Mars Liu <mars.liu@outlook.com>
2020-08-04 14:29:52 +0800

    use is true

--- src/main/scala/sisp/ast/If.scala
+++ src/main/scala/sisp/ast/If.scala
@@ -10,22 +10,21 @@ import sisp.ParserException
  * @since 2020/07/28 17:48
  */
 class If extends Lambda {
-  val isTrue = new IsTrue
 
   override def apply(env: Env, params: Seq[Any]): Either[Exception, Any] = {
     if (params.size < 2 || params.size > 3) {
       return Left(new ParserException(s"invalid if statement (if $params), parameters size should be 2 or 3"))
     }
-    isTrue.apply(env, Seq(params.head)) flatMap { cond =>
-      if (cond) {
-        env.eval(params(1))
+
+    if (IsTrue.isTrue(params.head)) {
+      env.eval(params(1))
+    } else {
+      if (params.size == 3) {
+        env.eval(params(2))
       } else {
-        if (params.size == 3) {
-          env.eval(params(2))
-        } else {
-          Right(Nil)
-        }
+        Right(Nil)
       }
+
     }
   }
 }
